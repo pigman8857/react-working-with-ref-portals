c0746617f2a87c74b517a3432bd7c98a29bfbfce
SomkamolV <somkamolv@hotmail.com>
2025-04-22 18:58:32 +0700

    section/131-139 => Try use Refs for more than DOM element connection.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player.jsx';
+import TimerChallenge from './components/TimerChallenge.jsx';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="Easy" targerTime={1}/>
+        <TimerChallenge title="Not easy" targerTime={5}/>
+        <TimerChallenge title="Getting tough" targerTime={10}/>
+        <TimerChallenge title="Pros only" targerTime={15}/>
+      </div>
     </>
   );
 }
