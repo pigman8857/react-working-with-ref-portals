419c1edd3e51891d891c3b23002495b1ef2d8786
SomkamolV <somkamolv@hotmail.com>
2025-04-22 17:17:14 +0700

    section/131-139 => 134 => introduce useRef to ref the input player name field and get its value to display when the button is clicked.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,24 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from 'react';
 
 export default function Player() {
+  const playerNameRef = useRef();
 
-  const [enteredPlayerName, setEnteredPlayerName] = useState("");
-  const [submitted, setSubmitted] = useState(false);
-
-  function handleChange(event) {
-    setSubmitted(() => false);
-    setEnteredPlayerName(() => event.target.value);
-  }
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
 
   function handleClick(event) {
-    setSubmitted(() => true);
+    setEnteredPlayerName(() => playerNameRef.current.value);
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredPlayerName : 'unknown entity'}</h2>
+      <h2>Welcome {enteredPlayerName  ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" onChange={handleChange}/>
+        <input 
+          ref={playerNameRef} 
+          type="text" 
+        />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
